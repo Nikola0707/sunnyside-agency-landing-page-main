7188934a00190a8f2b1ae0ed280a3e22db51adf6
Nikola0707 <ristoski.nikola@gmail.com>
2021-10-05 23:51:58 +0200

    Created NavBar and MainContent components

---
+++ src/Components/MainContent/MainContent.component.jsx
@@ -0,0 +1,23 @@
+import "./MainContent.style.css";
+const MainContent = () => {
+  return (
+    <div className="main-content-container">
+      <div>
+        <h2>Transform your brand</h2>
+        <p>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+          <a href="/"><span>LEARN MORE</span></a>
+        </p>
+      </div>
+      <div className="egg-image"></div>
+      <div className="glass-image"></div>
+      <div></div>
+      <div className="chery-image"></div>
+      <div className="orange-image"></div>
+    </div>
+  );
+};
+
+export default MainContent;
