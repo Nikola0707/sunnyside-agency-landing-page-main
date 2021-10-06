69713fcf6775ea28b9de34c75e9b5645e3605487
Nikola0707 <ristoski.nikola@gmail.com>
2021-10-06 14:20:38 +0200

    Implemented media query for mobile nav

--- src/Components/NavBar/NavBar.component.jsx
+++ src/Components/NavBar/NavBar.component.jsx
@@ -1,51 +1,69 @@
-import "./NavBar.style.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-scroll";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { useState } from "react";
+import "./NavBar.style.css";
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
-    <div className="navbar-container">
-      <div className="navbar-logo">
-        <Logo />
+    <div className="navbar">
+      <div className="mobile">
+        <div className="navbar-logo-mobile">
+          <Logo />
+        </div>
+        <GiHamburgerMenu
+          className="hamburger"
+          onClick={() => setShowLinks(!showLinks)}
+        />
       </div>
-      <div className="navbar-ul">
-        <ul>
-          <li>
-            <Link
-              activeClass="active"
-              to="about"
-              spy={true}
-              smooth={true}
-              duration={1500}
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              to="services"
-              spy={true}
-              smooth={true}
-              duration={1500}
-            >
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              smooth={true}
-              duration={1500}
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <button>CONTACT</button>
-          </li>
-        </ul>
+      <div
+        className={showLinks ? "navbar-container-mobile" : "navbar-container"}
+      >
+        <div className="navbar-logo">
+          <Logo />
+        </div>
+        <div className="navbar-ul">
+          <ul>
+            <div className="shape"></div>
+            <li>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                duration={1500}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="services"
+                spy={true}
+                smooth={true}
+                duration={1500}
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                duration={1500}
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <button>CONTACT</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
